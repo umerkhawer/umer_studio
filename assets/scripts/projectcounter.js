d2124993c8f514202c5abbb00dce31a5d3a02de6
umerkhawer <121026393+umerkhawer@users.noreply.github.com>
2025-07-10 15:50:26 +0500

    Add files via upload

---
+++ assets/scripts/projectcounter.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim3'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './assets/animations/Project Counter.json'
+});
---
+++ assets/scripts/toolset.js
@@ -0,0 +1,9 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim2'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: './assets/animations/Toolset.json'
+    });
+    
+    
